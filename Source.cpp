5a1631465f1933cfd3a52e1681edae1442f700e7
GabrielReyes04 <99066563+GabrielReyes04@users.noreply.github.com>
2022-02-05 00:33:44 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+//Gabriel Reyes
+//OCCC
+//Spring 2021
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float f, c; 
+	cout << "Enter temperature in Celsius:";
+	cin >> c;
+	f = (9.0/ 5.0 * c) + 32;
+	cout << "Temperature in Fahrenheit is:" << f; 
+
+	return 0;
+}
